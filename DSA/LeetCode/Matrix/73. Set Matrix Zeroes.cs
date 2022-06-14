2cb31d9c743c8452dd67c68463add0c0b56b82b1
SriniVasan.B <sritechnoo@gmail.com>
2022-06-14 21:13:32 +0530

    Adding SetMatrix Zero

---
+++ DSA/LeetCode/Matrix/73. Set Matrix Zeroes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.LeetCode.Matrix._73SetMatrixZeroes
+{
+    /*Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+    You must do it in place.
+
+
+
+    Example 1:
+
+    Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+    Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+    Example 2:
+
+    Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+
+
+    Constraints:
+
+        m == matrix.length
+        n == matrix[0].length
+        1 <= m, n <= 200
+        -231 <= matrix[i][j] <= 231 - 1
+
+
+
+    Follow up:
+
+        A straightforward solution using O(mn) space is probably a bad idea.
+        A simple improvement uses O(m + n) space, but still not the best solution.
+        Could you devise a constant space solution?
+
+    */
+
+    public class Solution
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            int rowLength = matrix.Length;
+            int colLength = matrix[0].Length;
+
+            HashSet<int> zeroRows = new HashSet<int>();
+            HashSet<int> zeroCols = new HashSet<int>();
+            for (int r = 0; r < rowLength; r++)
+            {
+                for (int c = 0; c < colLength; c++)
+                {
+                    if (matrix[r][c] == 0)
+                    {
+                        zeroRows.Add(r);
+                        zeroCols.Add(c);
+                    }
+                }
+            }
+
+            for (int r = 0; r < rowLength; r++)
+            {
+                for (int c = 0; c < colLength; c++)
+                {
+                    if (zeroRows.Contains(r) || zeroCols.Contains(c))
+                    {
+                        matrix[r][c] = 0;
+                    }
+                }
+            }
+
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using DSA.LeetCode.PriorityQueue._215KthLargestElementinanArray;
 
 namespace DSA
 {
@@ -7,9 +6,25 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            var result = new Solution().FindKthLargest(nums: new int[] { 3, 2, 1, 5, 6, 4 }, k: 2);
-
+            #region _215KthLargestElementinanArray
+            int[] nums = new int[] { 3, 2, 1, 5, 6, 4 };
+            int k = 2;
+            var instance = new DSA.LeetCode.PriorityQueue._215KthLargestElementinanArray.Solution();
+            var result = instance.FindKthLargest(nums: nums, k: k);
             Console.WriteLine(result);
+            #endregion
+
+            #region _73SetMatrixZeroes
+            int[][] matrix = new int[3][]
+                {
+                new int[] { 1, 1, 1 },
+                new int[] { 1, 0, 1 },
+                new int[] { 1, 1, 1 }
+                };
+            new DSA.LeetCode.Matrix._73SetMatrixZeroes.Solution().SetZeroes(matrix);
+            #endregion
+
+            Console.WriteLine("Hello World");
         }
     }
 }
